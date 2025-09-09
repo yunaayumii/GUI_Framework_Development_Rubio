2e74eea66505fa636a4d984db7bf8a5491234ca5
yuan <yuanmiguelrubio@gmail.com>
2025-09-09 18:05:45 +0800

    changed logic for the ghosting effect

--- draw/src/main/java/com/gabriel/draw/view/DrawingView.java
+++ draw/src/main/java/com/gabriel/draw/view/DrawingView.java
@@ -21,9 +21,10 @@ public class DrawingView extends JPanel {
 
     @Override
     public void paint(Graphics g) {
+        super.paint(g);
         Drawing drawing = (Drawing) appService.getModel();
         for(Shape shape : drawing.getShapes()){
-            shape.getRendererService().render(g, shape, true);
+            shape.getRendererService().render(g, shape, false);
             appService.setView(this);
         }
     }
