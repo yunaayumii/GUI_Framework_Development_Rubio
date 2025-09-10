afd33e8262993989d1d3434e94159a0e1b428eb3
yuan <yuanmiguelrubio@gmail.com>
2025-09-10 14:55:19 +0800

    added scaleCommand

--- draw/src/main/java/com/rubio/draw/command/scaleCommand.java
+++ draw/src/main/java/com/rubio/draw/command/scaleCommand.java
@@ -7,22 +7,31 @@ import com.gabriel.drawfx.service.AppService;
 import java.awt.*;
 
 public class scaleCommand implements Command {
-    public scaleCommand(AppService appService, Shape shape, Point newEnd) {
+    AppService appService;
+    Shape shape;
+    Point newEnd;
+    Point prevEnd;
 
+    public scaleCommand(AppService appService, Shape shape, Point newEnd) {
+        this.appService = appService;
+        this.shape = shape;
+        this.newEnd = newEnd;
+        this.prevEnd = shape.getEnd();
     }
 
     @Override
     public void execute() {
-
+        prevEnd = shape.getEnd();
+        shape.setEnd(newEnd);
     }
 
     @Override
     public void undo() {
-
+        shape.setEnd(prevEnd);
     }
 
     @Override
     public void redo() {
-
+        shape.setEnd(newEnd);
     }
 }
